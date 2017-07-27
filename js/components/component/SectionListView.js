f9383193ccac1d916ff76451ee4ad2f44c66233c
william <william.lee@gtedx.com>
2017-07-27 14:39:50 +0800

    study FlatList component

--- js/components/component/SectionListView.js
+++ js/components/component/SectionListView.js
@@ -5,6 +5,8 @@ import {View} from 'react-native';
  * Created by william on 17-7-27.
  *
  * https://reactnative.cn/docs/0.46/sectionlist.html
+ *
+ * http://www.jianshu.com/p/6302c4d48b97
  */
 export default class SectionListView extends Component {
 
